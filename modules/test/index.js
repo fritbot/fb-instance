948c3a183591b5f4e04af3c38970c9a2639dc4a7
Michael Pratt <urthen@gmail.com>
2015-03-31 12:58:14 -0500

    adding module pages test

--- modules/test/index.js
+++ modules/test/index.js
@@ -10,8 +10,24 @@ module.exports = {
 			'usage': 'test',
 			'trigger': /test/i,
 			'func': function (route, args) {
-				route.send("You tested it!")
+				route.send("You tested it!");
 			}
 		}
-	]
-}
+	],
+
+	web_init : function (web) {
+		var app = web.express();
+
+		// Set up views!
+		app.set('views', __dirname + '/templates');
+		app.set('view engine', 'jade');
+
+		app.get('/', web.render(app, 'test'));
+
+		app.get('/pants', web.render(app, 'pants', function(req, next) {
+			next({ pants : 'Yes'});
+		}));
+
+		web.addModuleApp('/test/', app, 'Test');
+	}
+};
